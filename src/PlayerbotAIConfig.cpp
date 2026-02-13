be1fd2d4823b6188ecfb974d6f72952536c53e43
Rikus Louw <rikuswlouw@gmail.com>
2026-02-13 10:53:59 +0200

    Removed whitespace

--- src/PlayerbotAIConfig.cpp
+++ src/PlayerbotAIConfig.cpp
@@ -183,7 +183,6 @@ bool PlayerbotAIConfig::Initialize()
     LoadSet<std::set<uint32>>(
         sConfigMgr->GetOption<std::string>("AiPlayerbot.AttunementQuests", "10279,10277,10282,10283,10284,10285,10296,10297,10298,11481,11482,11488,11490,11492,10901"),
         attunementQuests);
-
     botAutologin = sConfigMgr->GetOption<bool>("AiPlayerbot.BotAutologin", false);
     randomBotAutologin = sConfigMgr->GetOption<bool>("AiPlayerbot.RandomBotAutologin", true);
     minRandomBots = sConfigMgr->GetOption<int32>("AiPlayerbot.MinRandomBots", 500);
